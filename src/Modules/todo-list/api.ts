b9df7a9900bccf3c73b1b991e74282d549ff572b
Илья <ilya.panarin14@gmail.com>
2024-12-09 18:25:54 +0500

    Запросы к серверу

--- src/Modules/todo-list/api.ts
+++ src/Modules/todo-list/api.ts
@@ -1,5 +1,15 @@
 const BASE_URL = "http://localhost:3000";
 
+export type PaginatedResult<T> = {
+   data: T[];
+   first: number;
+   items: number;
+   last: number;
+   next: number | null;
+   pages: number;
+   prev: number | null;
+}
+
 export type TodoDto = {
    id: number,
    text: string,
@@ -9,9 +19,9 @@ export type TodoDto = {
 export const todoListApi = {
 
    //AbortSignal - позволяет отменить запрос, если он в какой-то момент оказался уже не нужен
-   getTodoList: ({signal}: {signal:AbortSignal}) => {
-      fetch(`${BASE_URL}/tasks`, {
+   getTodoList: ({page}: {page: number}, {signal}: {signal:AbortSignal}) => {
+      fetch(`${BASE_URL}/tasks?_page=${page}&_per_page=10`, {
          signal
-      }).then(res => res.json() as Promise<TodoDto[]>)
+      }).then(res => res.json() as Promise<PaginatedResult<TodoDto>>)
    }
 }
--- src/Modules/todo-list/todo-list.tsx
+++ src/Modules/todo-list/todo-list.tsx
@@ -1,16 +1,19 @@
+import { useState } from "react"; 
 import { useQuery } from "@tanstack/react-query";
 import { todoListApi } from "./api";
 
 export default function TodoList() {
 
+   const [page, setPage] = useState()
+
    //queryKey - это ключ помогает react-query отличать запросы между друг другом
    //queryKey: ['tasks', 'list'] - это массив, где 'tasks' - это имя ключа для этого запроса, 'list' - это имя для этого запроса в массиве ключей
 
    //queryFn - это любая асинхронная функция, которая возвращает Promise, который затем будет использоваться в качестве результата запроса
-   const {data, error, isPending} = useQuery({
-      queryKey: ["tasks", "list"], 
-      queryFn: todoListApi.getTodoList
-   })
+   const {data: todoItems, error, isPending} = useQuery({
+      queryKey: ["tasks", "list", {page}], 
+      queryFn: (meta) => todoListApi.getTodoList({page}, meta),
+   });
 
    if (isPending) return <div>Loading...</div>
 
@@ -21,11 +24,14 @@ export default function TodoList() {
          <h1 className="text-3xl font-bold underline mb-5">Todo List</h1>
 
          <div className="flex flex-col gap-4">
-            {data.map(todo => (
-               <div className="border border-slate-300 rounded p-3" key={todo.id}> {todo.text}</div>
+            {todoItems.data.map(todo => (
+               <div className="border border-slate-300 rounded p-3" key={todo.id}>{todo.text}</div>
             ))}
          </div>
-
+         <div className=" flex gap-2 mt-4">
+            <button onClick={() => setPage(p => Math.max(p - 1, 1)} className="p-3 rounded border border-teal-500">prev</button>
+            <button onClick={() => setPage(p => Math.min(p + 1, todoItems.pages)} className="p-3 rounded border border-teal-500">next</button>
+         </div>
       </div>
    )
 }

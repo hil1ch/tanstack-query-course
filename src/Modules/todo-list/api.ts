c3323916fab64e7402b3ef793451114d8d32195e
Илья <ilya.panarin14@gmail.com>
2024-12-27 15:51:09 +0500

    Создание Todo

--- src/Modules/todo-list/api.ts
+++ src/Modules/todo-list/api.ts
@@ -12,29 +12,50 @@ export type PaginatedResult<T> = {
 }
 
 export type TodoDto = {
-   id: number,
+   id: string,
    text: string,
    done: boolean,
+   userId: string
 }
 
 export const todoListApi = {
+   baseKey: 'tasks',
 
    //AbortSignal - позволяет отменить запрос, если он в какой-то момент оказался уже не нужен
 
-   getTodoListQueryOptions: ({page}: {page: number}) => {
+   getTodoListQueryOptions: () => {
       return queryOptions({
-         queryKey: ["tasks", "list", {page}], 
-         queryFn: (meta) => jsonApiInstance<PaginatedResult<TodoDto>>(`/tasks?_page=${page}&_per_page=10`, {signal: meta.signal}),
+         queryKey: [todoListApi.baseKey, "list"], 
+         queryFn: (meta) => jsonApiInstance<TodoDto[]>(`/tasks`, {signal: meta.signal}),
       });
    },
 
    getTodoListInfinityQueryOptions: () => {
       return infiniteQueryOptions({
-         queryKey: ["tasks", "list"], 
+         queryKey: [todoListApi.baseKey, "list"], 
          queryFn: (meta) => jsonApiInstance<PaginatedResult<TodoDto>>(`/tasks?_page=${meta.pageParam}&_per_page=10`, {signal: meta.signal}),
          initialPageParam: 1,
          getNextPageParam: (result) => result.next,
          select: result => result.pages.flatMap(page => page.data)
       });
-   }
+   },
+
+   createTodo: (data: TodoDto) => {
+      return jsonApiInstance<TodoDto>(`/tasks`, {
+         method: 'POST',
+         json: data
+      })
+   },
+
+   updateTodo: (id: string, data: Partial<TodoDto>) => {
+      return jsonApiInstance<TodoDto>(`/tasks/${id}`, {
+         method: 'PATCH',
+         json: data
+      })
+   },
+   deleteTodo: (id: string) => {
+      return jsonApiInstance(`/tasks/${id}`, {
+         method: 'DELETE',
+      })
+   },
 }
--- src/Modules/todo-list/todo-list.tsx
+++ src/Modules/todo-list/todo-list.tsx
@@ -1,8 +1,8 @@
+import { useCreateTodo } from "./useCreateTodo";
 import { useTodoList } from "./useTodoList";
 
-
 export default function TodoList() {
-   const {cursor, error, isLoading, todoItems} = useTodoList();
+   const {error, isLoading, todoItems} = useTodoList();
 
    //queryKey - это ключ помогает react-query отличать запросы между друг другом
    //queryKey: ['tasks', 'list'] - это массив, где 'tasks' - это имя ключа для этого запроса, 'list' - это имя для этого запроса в массиве ключей
@@ -13,7 +13,9 @@ export default function TodoList() {
 
       // placeholderData - это данные, которые будут показаны во время загрузки запроса. Это может помочь избежать пустого экрана при загрузке
       // keepPreviousData - предыдущие данные, показывающиеся при загрузке страницы
-      
+     
+   const createTodo = useCreateTodo();
+   
 
    if (isLoading) return <div>Loading...</div>
 
@@ -25,12 +27,15 @@ export default function TodoList() {
       <div className="p-5 mx-auto max-w-[1200px] mt-10">
          <h1 className="text-3xl font-bold underline mb-5">Todo List</h1>
 
+         <form className="flex gap-2 mb-5" onSubmit={createTodo.handleCreate}>
+            <input className="rounded p-2 border border-teal-500" type="text" name="text"></input>
+            <button disabled={createTodo.isPending} className="rounded p-2 border border-teal-500 disabled:opacity-50">Создать</button>
+         </form>
          <div className={"flex flex-col gap-4"}>
             {todoItems?.map(todo => (
                <div className="border border-slate-300 rounded p-3" key={todo.id}>{todo.text}</div>
             ))}
          </div>
-         {cursor}
       </div>
    )
 }
